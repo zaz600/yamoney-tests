21e4ab2d93cd65b0b396c91028bd0cbfa19d48b5
Dmitriy Denisov <zaz600@gmail.com>
2019-02-26 07:21:15 +0300

    fix CurrentUrlChecker.shouldContains not shown current url if failed

--- src/test/kotlin/ru/yandex/money/qa/tests/utils/CurrentUrlChecker.kt
+++ src/test/kotlin/ru/yandex/money/qa/tests/utils/CurrentUrlChecker.kt
@@ -2,29 +2,26 @@ package ru.yandex.money.qa.tests.utils
 
 import io.qameta.allure.Step
 import org.openqa.selenium.WebDriver
+import org.openqa.selenium.support.ui.ExpectedConditions
 import org.openqa.selenium.support.ui.WebDriverWait
+
 
 /**
  * Сахар для проверки текущего URL-а
  */
 class CurrentUrlChecker(val driver: WebDriver) {
-    @Step("Проверка содержимого URLа браузера")
-    private fun checkCurrentUrl(timeoutSec: Long = 30, checker: (String) -> Boolean) {
-        WebDriverWait(driver, timeoutSec).until { checker(it.currentUrl) }
-    }
-
     @Step("Проверка, что URL браузера содержит подстроку")
     fun shouldContains(partUrl: String, timeoutSec: Long = 30) {
-        checkCurrentUrl(timeoutSec) { it.contains(partUrl) }
+        WebDriverWait(driver, timeoutSec).until(ExpectedConditions.urlContains(partUrl))
     }
 
     @Step("Проверка, что URL браузера оканчивается подстрокой")
     fun shouldEndsWith(partUrl: String, timeoutSec: Long = 30) {
-        checkCurrentUrl(timeoutSec) { it.endsWith(partUrl) }
+        WebDriverWait(driver, timeoutSec).until(ExpectedConditions.urlMatches(".*?$partUrl\$"))
     }
 
     @Step("Проверка, что URL браузера равен строке")
     fun shouldBeEqualsTo(url: String, timeoutSec: Long = 30) {
-        checkCurrentUrl(timeoutSec) { it == url }
+        WebDriverWait(driver, timeoutSec).until(ExpectedConditions.urlToBe(url))
     }
 }
